49c6dc5a93e81d1952aa8328e2aad02d3bb79531
Manu <git@manuelgrabowski.de>
2020-12-14 14:15:19 +0100

    Update notification.go

--- notification/notification.go
+++ notification/notification.go
@@ -2,5 +2,5 @@ package notification
 
 // Repository is an interface for a notifier repository
 type Repository interface {
-	Post(text string, author string, url string) error
+	Post(gofeed.Item item) error
 }
