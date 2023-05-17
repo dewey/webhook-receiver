34ee5c460eb6294dccad8ed39a02ee3d31743653
Philipp Defner <philipp.defner@justwatch.com>
2023-05-17 21:30:31 +0200

    Fix

--- notification/mock_notification.go
+++ notification/mock_notification.go
@@ -1,10 +1,7 @@
 package notification
 
 import (
-	"bytes"
 	"context"
-	"strings"
-
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 )
@@ -27,26 +24,6 @@ func (s *mockRepository) String() string {
 }
 
 func (s *mockRepository) Post(ctx context.Context, text string, author string, url string) error {
-	// We split into words, and add as many words while trying to stay roughly under 100 characters for the post
-	var length int
-	var summaryTokens []string
-	for _, t := range strings.Split(text, " ") {
-		runes := bytes.Runes([]byte(t))
-		// If length of toot is below 100, we add tokens to the list
-		if length <= 100 {
-			length += len(runes)
-			summaryTokens = append(summaryTokens, t)
-		} else {
-			// Only if the word is shorter than 5 do we also still add it to the list
-			if len(runes) < 5 {
-				summaryTokens = append(summaryTokens, t)
-				length += len(runes)
-				break
-			} else {
-				break
-			}
-		}
-	}
 	level.Info(s.l).Log("msg", "mocked notification successfully sent", "id", 123, "url", "https://example.com/123")
 	return nil
 }
